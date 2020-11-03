335998f5a6e5949cb251e437bd2eaf6ff5c3c647
zxt1996 <15625144271@163.com>
2020-11-03 16:30:07 +0800

    函数

--- src/main/main.go
+++ src/main/main.go
@@ -74,6 +74,33 @@ func showMemoryAddress(x *int)  {
 
 const change string = "不能改变的常量"
 
+//返回多个值
+func moreNum() (int, string)  {
+	one := 2
+	two := "jojo"
+
+	return one, two
+}
+
+//不定参数
+func forMore(number... int)  {
+	fmt.Println(number)
+}
+
+//具名返回值
+//在终止语句return前给具名变量进行了赋值。使用具名返回值时，无须显式地返回相应的变量
+func knowName() (x, y string) {
+	x = "jojo"
+	y = "ok"
+
+	return
+}
+
+//将函数做为值传递
+func anotherFunction(f func() string) string  {
+	return f()
+}
+
 func main()  {
 	aboutType()
 	variable()
@@ -83,4 +110,16 @@ func main()  {
 	showMemoryAddress(&i)
 
 	fmt.Println(change)
+
+	one, two := moreNum()
+	fmt.Println(one, two)
+
+	forMore(1, 2, 3)
+	fmt.Println(knowName())
+
+	fn := func() string {
+		return "jojo"
+	}
+
+	fmt.Println(anotherFunction(fn))
 }
