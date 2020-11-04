daa0341b3e92ed5f9111f0edb7517d00856eab41
zxt1996 <15625144271@163.com>
2020-11-04 11:07:52 +0800

    错误处理

--- src/main/main.go
+++ src/main/main.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"strconv"
 	"math"
+	"errors"
 )
 
 //强类型
@@ -349,6 +350,49 @@ func measure(g geometry)  {
 	fmt.Println(g.per())
 }
 
+//错误处理
+func f1(arg int) (int, error)  {
+	if arg == 42 {
+		//使用errors.New 可返回一个错误信息
+		return -1, errors.New("can't work with 42")
+	}
+
+	return arg + 3, nil
+}
+
+type argError struct {
+	arg int
+	prob string
+}
+
+//fmt包在处理error时会调用Error方法
+func (e *argError) Error() string  {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
+
+func f2(arg int) (int, error)  {
+	if arg == 42 {
+		return -1, &argError{arg, "can't work with it"}
+	}
+	return arg + 3, nil
+}
+
+func myErr()  {
+	for _, i := range [] int{7, 42} {
+		if r, e := f1(i); e != nil {
+			fmt.Println("f1 failed:", e)
+		} else {
+			fmt.Println("f1 worked:", r)
+		}
+
+		if r, e := f2(i); e != nil {
+			fmt.Println("f2 failed:", e)
+		} else {
+			fmt.Println("f2 worked:", r)
+		}
+	}
+}
+
 func main()  {
 	aboutType()
 	variable()
@@ -391,4 +435,6 @@ func main()  {
 		name: "zheling",
 	}
 	measure(nowRect)
+
+	myErr()
 }
