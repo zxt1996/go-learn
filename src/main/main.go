0ebb0e4162a4f562a873598b07b5eae30b5faa33
zxt1996 <15625144271@163.com>
2020-11-04 10:21:09 +0800

    接口

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"math"
 )
 
 //强类型
@@ -323,6 +324,31 @@ func aboutChange()  {
 	fmt.Println(now)
 }
 
+//接口
+type geometry interface {
+	area() string
+	per() float64
+}
+
+type rect struct {
+	width, height float64
+	name string
+}
+
+func (r rect) area() string  {
+	return "here is " + r.name
+}
+
+func (r rect) per() float64  {
+	return math.Pi * r.height * r.width
+}
+
+func measure(g geometry)  {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.per())
+}
+
 func main()  {
 	aboutType()
 	variable()
@@ -358,4 +384,11 @@ func main()  {
 	aboutWay()
 
 	aboutChange()
+
+	nowRect := rect{
+		width: 1.2,
+		height: 2.3,
+		name: "zheling",
+	}
+	measure(nowRect)
 }
