f900906403661989c4f32121fb92a535e4ba560e
zxt1996 <15625144271@163.com>
2020-11-05 10:11:10 +0800

    控制流程

--- src/main/main.go
+++ src/main/main.go
@@ -8,56 +8,9 @@ import (
 	"myType"
 	"myPointer"
 	"myFunc"
+	"myControl"
 )
 
-
-
-
-//控制流程
-func controlFlow()  {
-	can := true
-
-	//if语句
-	if can {
-		fmt.Println("可以")
-	}
-
-	two := 12
-
-	if two < 10 {
-		fmt.Println("小于10")
-	} else if two < 12 {
-		fmt.Println("小于12")
-	} else {
-		fmt.Println("其他")
-	}
-
-	var all int
-	for i := 0; i < 10; i++ {
-		all += i
-	}
-
-	fmt.Println(all)
-
-	//遍历数组
-	numbers := []int {1, 2, 3}
-
-	for index, value := range numbers{
-		fmt.Println("numbers[", index, "]: ", value)
-	}
-}
-
-//defer语句
-//在函数返回前执行另一个函数
-//通常用于执行清理操作或确保操作（如网络调用）完成后再执行另一个函数
-func aboutDe()  {
-	fmt.Println("start")
-
-	defer fmt.Println("one")
-	defer fmt.Println("two")
-
-	fmt.Println("end")
-}
 
 //数组、切片和映射
 func gather()  {
@@ -400,9 +353,7 @@ func main()  {
 
 	myFunc.MyFunc()
 
-	controlFlow()
-
-	aboutDe()
+	myControl.MyControl()
 
 	gather()
 
